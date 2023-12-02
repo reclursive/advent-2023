fa6a9ac77e6407294699d61b4b0d8b473eecec56
reclursive <imafraidofferrets@gmail.com>
2023-12-01 20:02:44 -0500

    cleaned up, dictionary instead of tally, answer submitted

--- advent/day_1.py
+++ advent/day_1.py
@@ -2,28 +2,25 @@ file = open("input/no_1.txt")
 test = file.read()
 
 def trebuchet(input):
-    x= slice(0, 10)
-    container = input.split("\n")[x]
+    container = input.split("\n")
 
     dict = {}
     no_calib = 0
 
     if len(container) >= 1:
-        # total = 0
         for string in container:
             num_string = list(filter(str.isdigit, string))
-            if len(num_string) <= 1:
+            key = string
+            if len(num_string) < 1:
                 no_calib +=1
-                continue
-            else:
-                key = string
-                end = len(num_string)-1
-                combo = int(num_string[0]+num_string[end])
+            elif len(num_string) ==1:
+                combo = int(num_string[0]+num_string[0])
                 dict[key] = combo
-
-    print(sum(dict.values()))
-    print(dict)
-    print(no_calib)
+            else:
+                end = len(num_string)-1
+                combo_2 = int(num_string[0]+num_string[end])
+                dict[key] = combo_2
+                
 
     return sum(dict.values())
 
