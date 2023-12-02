4b102c66d102a724e5e9811dbdcec0af874474c8
reclursive <imafraidofferrets@gmail.com>
2023-12-01 21:53:44 -0500

    replaced concats w joins

--- advent/day_1.py
+++ advent/day_1.py
@@ -1,3 +1,31 @@
+# file = open("input/no_1.txt")
+# test = file.read()
+
+# def trebuchet(input):
+#     container = input.split("\n")
+
+#     dict = {}
+
+#     if len(container) >= 1:
+#         for string in container:
+#             num_string = list(filter(str.isdigit, string))
+#             key = string
+#             if len(num_string) < 1:
+#                 continue
+#             elif len(num_string) ==1:
+#                 combo = int(num_string[0]+num_string[0])
+#                 dict[key] = combo
+#             else:
+#                 end = len(num_string)-1
+#                 combo_2 = int(num_string[0]+num_string[end])
+#                 dict[key] = combo_2
+                
+
+#     return sum(dict.values())
+
+# trebuchet(test)
+
+
 file = open("input/no_1.txt")
 test = file.read()
 
@@ -5,27 +33,27 @@ def trebuchet(input):
     container = input.split("\n")
 
     dict = {}
-    no_calib = 0
 
     if len(container) >= 1:
         for string in container:
             num_string = list(filter(str.isdigit, string))
             key = string
             if len(num_string) < 1:
-                no_calib +=1
+                continue
             elif len(num_string) ==1:
-                combo = int(num_string[0]+num_string[0])
-                dict[key] = combo
+                temp = num_string*2
+                combo = "".join(temp)
+                dict[key] = int(combo)
             else:
                 end = len(num_string)-1
-                combo_2 = int(num_string[0]+num_string[end])
-                dict[key] = combo_2
+                combo_2 = num_string[::len(num_string)-1]
+                final_num = "".join(combo_2)
+                dict[key] = int(final_num)
                 
-
-    return sum(dict.values())
+    
+    print(sum(dict.values()))
 
 trebuchet(test)
-
 
 
 
