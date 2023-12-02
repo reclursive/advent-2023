0da2382f1c36f81785d1a458923d61ae7cdec406
reclursive <imafraidofferrets@gmail.com>
2023-12-01 19:14:52 -0500

    accounted for 1 digit present

--- advent/day_1.py
+++ advent/day_1.py
@@ -1,31 +1,31 @@
 file = open("input/no_1.txt")
 test = file.read()
 
-def trebuchet(calib):
-    cut = slice(0, 10)
-    container = calib.split("\n")[cut]
-    print(container)
+def trebuchet(input):
+    x= slice(0, 10)
+    container = input.split("\n")[x]
 
-    nums =[]
+    dict = {}
+    no_calib = 0
+
     if len(container) >= 1:
-        total = 0
+        # total = 0
         for string in container:
             num_string = list(filter(str.isdigit, string))
-            if len(num_string) == 0:
+            if len(num_string) <= 1:
+                no_calib +=1
                 continue
-            elif len(num_string)==1:
-                total = total + int(num_string[0])
             else:
+                key = string
                 end = len(num_string)-1
                 combo = int(num_string[0]+num_string[end])
-                total = total + combo
+                dict[key] = combo
 
-    print(nums)
-    print(total)
+    print(sum(dict.values()))
+    print(dict)
+    print(no_calib)
 
-    return total
-
-
+    return sum(dict.values())
 
 trebuchet(test)
 
