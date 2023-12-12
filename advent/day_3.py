9f868bc2cbfa20848eb82b8e088c1303cec337c1
reclursive <imafraidofferrets@gmail.com>
2023-12-11 23:57:59 -0500

    day three partial clean, day four files create

--- advent/day_3.py
+++ advent/day_3.py
@@ -1,136 +1,64 @@
-
-# file = open("input/tester.txt")
-# test = file.read()
-# import re
-# input_2 = test.lower()
-# container = input_2.split("\n")
-
-
-# def circle_scan(line, char_idx):
-#     pass
-#     # if line = 0:
-#     # elif line = len(input-1):
-#     # else:
-
-#     # line_up = line - 1
-#     # line_down = line + 1
-#     # right = char_idx+1
-#     # left = char_idx-1
-
-# def gondola(input):
-#     # regex_1 = [^\d.]
-#     engine_map = {}
-#     # print(input)
-#     tally = 0
-    
-#     for index, line in enumerate(input):
-#         # pass
-#         # line_2 = line.replace([^a-zA-Z\d\s\.], "bar")
-#         auto_bings = re.sub("[^0-9a-zA-Z\.]", "BING", line)
-#         line_3 = re.split(r"[^0-9a-zA-Z$]", auto_bings)
-
-#         print(len(line_3), "LINEs")
-
-#         for idx, char in enumerate(line_3):
-#             print(char, idx)
-#             if char == '':
-#                 continue
-#             elif char.isalnum():
-#                 if len(char) == 1:
-#                     if line[index-1][idx] == "BING" or line[index+1][idx] == "BING":
-#                         tally += char
-#                     else:
-#                         continue
-#                 elif len(char)>2 and idx> 0 and index>1:
-#                     diag_FF = idx +len(char)
-#                     diag_RW = idx -1 
-#                     if line[index-1][diag_FF] == "BING" or line[index+1][diag_RW] == "BING" or line[index+1][diag_FF] == "BING" or line[index-1][diag_RW] == "BING" or line[index-1][diag_FF] =="BING":
-#                         tally += char
-#                     else:
-#                         splitty = char.split("BING")
-#                         print(splitty)
-#                         print(char, "CLEAR NUMB")
-
-#             else:
-#                 splitty = char[idx].split("BING")
-#                 print(splitty, "splitty")
-#         print(tally, "tally")
-#         # print(line_3)
-#         # print(line_2, index)
-#         # for idx, char in enumerate(line_3):
-#         #     # print(char, idx)
-#         #     if char.isdigit():
-#         #         print(char)
-#         #         # num_len = len(line[index])
-#         #         print("digit", index)
-#         #     else:
-#         #         print("empty", index)
-#         # print(index, line)
-#         # for index, char in enumerate(line):
-#         #     print(index, char)
-
-# gondola(container)
-
-
 file = open("input/tester.txt")
 test = file.read()
 import re
 input_2 = test.lower()
 container = input_2.split("\n")
-
 engine_map = {}
-def circle_scan(array, x, y):
-    if len(array) == 2 and array[0].isdigit() or array[1].isdigit():
-        tally += int(array[0] or 0)
-        tally += int(array[1] or 0)
-    elif array == ["", ""]:
-        engine_map["BING"] = tuple(x, y)
-    elif len(array) == 1 and array[0].isdigit():
-        engine_map[array[0]] == tuple(x, y)
-    print(engine_map)
+tally = 0
+bing_count = 0
 
+def fast_filt(pair):
+    key, value = pair
+    if "B" in key:
+        return True
+    else:
+        return False 
+
+def compare_remaining(remain_dict):
+    global tally
+    bingo = dict(filter(fast_filt, remain_dict.items()))
+    print(bingo)
+    for key,value in remain_dict.items():
+        if key.isnumeric():
+            length = len(key) +2
+            upper_diag = (value[0]-1, value[1]-1) or (value[0]-1, [i for i in range(value[1], value[1]+length)])
+            lower_diag =(value[0]+1, value[1]-1) or (value[0]+1, [i for i in range(value[1], value[1]+length)])
+            print(key, value)
+            if lower_diag in bingo.values() or upper_diag in bingo.values():
+                tally += int(key)
+                print("HITTT", key)
+                continue
+
+def circle_scan(array, x, y):
+    global tally
+    global bing_count
+    
+    if len(array) == 2:
+        if array[0].isdigit() and array[1].isdigit():
+            tally += int(array[0]) +int(array[1])
+            array = ' '.join(array)
+            tally += int(array[0]) + int(array[1])
+        else:
+            bing_count += 1
+            engine_map["B"+str(bing_count)] = (x, y)
+    elif len(array) == 1 and array[0].isdigit():
+        num = array[0]
+        engine_map[num] = (x, y)
+
+    return compare_remaining(engine_map)
 
 def gondola(input):
-    # regex_1 = [^\d.]
-    # print(input)
-    tally = 0
-    
     for index, line in enumerate(input):
-        # pass
-        # line_2 = line.replace([^a-zA-Z\d\s\.], "bar")
-        auto_bings = re.sub("[^0-9a-zA-Z\.]", "BING", line)
+        auto_bings = re.sub("[^0-9a-zA-Z\.]", "B", line)
         line_3 = re.split(r"[^0-9a-zA-Z$]", auto_bings)
-
-        print(len(line_3), "LINEs")
-
         for idx, char in enumerate(line_3):
-            print(char, idx)
+            print(idx, index, char, "MEEP")
             if char == '':
                 continue
             elif char is not char.isalnum():
-                part_nums = re.split("BING", char)
+                print("SPLITTY", char)
+                part_nums = re.split("B", char)
                 print(part_nums, "PN")
                 circle_scan(part_nums, index, idx)
-
-                
-
-        print(tally, "tally")
-        # print(line_3)
-        # print(line_2, index)
-        # for idx, char in enumerate(line_3):
-        #     # print(char, idx)
-        #     if char.isdigit():
-        #         print(char)
-        #         # num_len = len(line[index])
-        #         print("digit", index)
-        #     else:
-        #         print("empty", index)
-        # print(index, line)
-        # for index, char in enumerate(line):
-        #     print(index, char)
-
-
-
-            
-
+   
 gondola(container)
