926de16b42b8f72c4d1600f31d52b4094b1f4aff
reclursive <imafraidofferrets@gmail.com>
2023-12-13 00:51:32 -0500

    index fix after split

--- advent/day_3.py
+++ advent/day_3.py
@@ -1,4 +1,4 @@
-file = open("input/no_3.txt")
+file = open("input/tester.txt")
 test = file.read()
 import re
 input_2 = test.lower()
@@ -28,11 +28,14 @@ def compare_remaining(remain_dict):
         lower_diag = (lower, right)
         lower_left = (lower, left)
         if "m" in key:
-            dup = key.split("m")[0]
+            dup = key.split("m")
             tally += int(dup[0])
+            print("HIT 3 multiple", dup)
+            continue
         elif key.isnumeric():
             if lower_diag in bingo.values() or upper_diag in bingo.values() or upper_left in bingo.values() or lower_left in bingo.values():
                 tally += int(key) 
+                print("HIT SURROUNDING", key)
                 continue
             else:
                 continue
@@ -40,6 +43,7 @@ def compare_remaining(remain_dict):
             if lower_diag in bingo.values() or upper_diag in bingo.values() or upper_left in bingo.values() or lower_left in bingo.values():
                 dup = int(key.split("m")[0])
                 tally += dup
+                print("HIIT EMPTY 1")
                 continue
 
 
@@ -51,43 +55,50 @@ def circle_scan(array, x, y):
     global engine_map
     global tally
     global bing_count
-    if array[0] == 'B':
+                
+    if len(array)== 2 and (array[0].isnumeric() or array[1].isnumeric()):
+        dif= "m" + str(y+x)
+        if array[0] in engine_map.keys():
+            engine_map[array[0] + dif] = (x, y)
+        if array[1] in engine_map.keys():
+            engine_map[array[1] + dif] = (x, y)
+    elif len(array)==1 and array[0] == 'B':
         bing_count += 1
         engine_map["B"+str(bing_count)] = (x, y)
-    if len(array) == 2:
-        if array[0].isdigit() and array[1].isdigit():
+    if len(array)==2 and array[1] == 'B'or array[0]== 'B':
+        bing_count += 1
+        engine_map["B"+str(bing_count)] = (x, y)
+    elif len(array) == 2 and array[0].isdigit() and array[1].isdigit():
             tally += int(array[0]) +int(array[1])
-        elif array[1].isnumeric():
-            tally += int(array[1])
-    elif len(array) == 1 and array[0].isnumeric():
-        if array[0] in engine_map.keys():
-            dif = "m"+ str(y+x)
-            engine_map[array[0] + dif] = (x, y)
-        else:
-            engine_map[array[0]] = (x, y)
+    engine_map = engine_map
     return engine_map
 
 def gondola(input):
     global engine_map
     global tally
     for index, line in enumerate(input):
+        print("LINE",len(line))
         auto_bings = re.sub("[^0-9a-zA-Z\.]", "B", line)
         line_3 = re.split(r"[^0-9a-zA-Z]", auto_bings)
+        print("AFTER split", len(line_3))
         for idx, char in enumerate(line_3):
             if char == '':
                 continue
             elif len(char)>1 and char[0] == 'B':
                 part_numsB = char.split("B")
-                engine = circle_scan(part_numsB, index, idx)
-            elif char == 'B':
+                circle_scan(part_numsB, index, idx)
+            elif char[0] == 'B':
                 part_numsC = ['B']
-                engine = circle_scan(part_numsC, index, idx)
+                circle_scan(part_numsC, index, idx)
             elif char is not char.isalnum():
                 part_numsN = re.split("B", char)
-                engine = circle_scan(part_numsN, index, idx)
+                circle_scan(part_numsN, index, idx)
             else:
                 continue
-    return compare_remaining(engine)
+            print("mor", index, char, idx)
+        print("mor OUTER")
+
+    return compare_remaining(engine_map)
    
 print(gondola(container))
 
