b240f5eeff49c4e3760cdeafadc945e206ced8e4
reclursive <imafraidofferrets@gmail.com>
2023-12-17 00:40:18 -0500

    gold star achieved, passes all tests. Removed unecessary prints

--- advent/day_4.py
+++ advent/day_4.py
@@ -1,11 +1,11 @@
-file = open("input/tester.txt")
+file = open("input/no_4.txt")
 test = file.read()
 import re
 input_2 = test.lower()
 container = input_2.split("\n")
+total_tally = 0
 
-
-#oop approach  pseudo
+#oop approach 
 #first sort with winning ticket
 #then sort hand 
 # then apply check only while within range to speed up process
@@ -16,9 +16,9 @@ class Lottery:
     def __init__(self, bundle):
         self.bundle = bundle
         self.length = len(bundle)
-        total_tally = 0
 
     def card_compare(self):
+        global total_tally
         for card in self.bundle:
             card_wins = 0
             c_and_h = card.split(" | ")
@@ -28,29 +28,27 @@ class Lottery:
             winning_nums2 = winning_nums.split(" ")
             winning_final = sorted(list(filter(None, winning_nums2)), key=float)  
 
-            print(winning_final)
-            print(main_hand)
             for num in main_hand:
                 if int(num) < int(winning_final[0]):
                     continue
                 elif int(num) <= int(winning_final[-1]):
-                    print(card_wins, "hiii") 
                     if num in winning_final:
                         if card_wins == 0 or card_wins ==1:
                             card_wins += 1
-                            print(num, "initial hits")
                         else:
-                            card_wins = card_wins*card_wins     
-                            print(num, "double up")  
-                else:
-                    continue
+                            card_wins = card_wins*2     
+                    else:
+                        continue
+            total_tally += card_wins
+        return total_tally
+
 
          
 
 powerBall = Lottery(container)
 
-powerBall.card_compare()
-#  powerBall = Lottery(winning_final, hand , check())
+print(powerBall.card_compare())
+
 
 
 
