b8024c58e0d16834498f9a6cc37c327515f12e9e
reclursive <imafraidofferrets@gmail.com>
2023-12-16 23:59:22 -0500

    testing lines added and logic situated, perfect execution for small samples, not accounting for dupes in main hand

--- advent/day_4.py
+++ advent/day_4.py
@@ -16,10 +16,11 @@ class Lottery:
     def __init__(self, bundle):
         self.bundle = bundle
         self.length = len(bundle)
+        total_tally = 0
 
     def card_compare(self):
-        print(self.length)
         for card in self.bundle:
+            card_wins = 0
             c_and_h = card.split(" | ")
             card_num = c_and_h[0]
             main_hand = sorted(list(filter(None, c_and_h[1].split(" "))), key=float)  
@@ -30,21 +31,19 @@ class Lottery:
             print(winning_final)
             print(main_hand)
             for num in main_hand:
-                card_wins = 0
-                if num <= winning_final[0]:
-                    pass
-                elif num <= winning_final[-1]:
+                if int(num) < int(winning_final[0]):
+                    continue
+                elif int(num) <= int(winning_final[-1]):
+                    print(card_wins, "hiii") 
                     if num in winning_final:
                         if card_wins == 0 or card_wins ==1:
                             card_wins += 1
-                            print(card_wins, num, "ADDED")
+                            print(num, "initial hits")
                         else:
-                            card_wins = card_wins*card_wins
-                            print(card_wins, num, "ADDED MORE ONE")
-                            
+                            card_wins = card_wins*card_wins     
+                            print(num, "double up")  
                 else:
                     continue
-
 
          
 
